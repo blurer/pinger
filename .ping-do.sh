e7ef182a51638ee8c859197e066e9b1ef49c9e75
bryan <blurer@pm.me>
2021-07-31 09:45:17 +0900

    fixed do-ping

--- .ping-do.sh
+++ .ping-do.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# taken from: http://speedtest-sfo1.digitalocean.com/
 
-for DC in NYC1 NYC2 NYC3 SFO1 SFO2 SFO3 TOR1 LON1 FRA1 AMS2 AMS3 SGP1 BLR1
+for DC in nyc1 nyc2 nyc3 sfo1 sfo2 sfo3 tor1 lon1 fra1 ams2 ams3 sgp1 blr1
 do
     printf "$DC: \t$(ping -i .2 -c 10 -q speedtest-$DC.digitalocean.com | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
 done
